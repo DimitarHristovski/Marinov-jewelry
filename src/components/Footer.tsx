49370904c1c9009b3f0d9c12365c0b70c865381a
ThekoMK <nikolovski.dejvid@yahoo.com>
2023-12-23 16:25:59 +0100

    finished our story

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container-fluid text-darkgreen">
+    <div className="text-darkgreen">
       <div className="row">
         <div className="col-12 position-relative  ">
           <div className="position-absolute d-flex flex-column content-display align-content-around ">
